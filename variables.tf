c6cd93d9044fe864858caf099bb18422952ae474
varun <varunvarneker06@gmail.com>
2026-03-10 19:32:11 +0530

    Fix Azure region

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "location" {
+  type    = string
   default = "centralindia"
 }
 
 variable "resource_group_name" {
+  type    = string
   default = "terraform-demo-rg"
 }
 
 variable "admin_username" {
+  type    = string
   default = "azureuser"
 }
 
 variable "admin_password" {
+  type    = string
   default = "Password@1234"
 }
